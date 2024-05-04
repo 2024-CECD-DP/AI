3716f79770bf92666990832c7ff4475fb006faf7
Youngha Park <youngha@Younghas-MacBook-Pro.local>
2024-05-04 15:15:35 +0900

    update get_db_connection

--- fast_api_server/utils.py
+++ fast_api_server/utils.py
@@ -1,12 +1,12 @@
-import config
+from config import PostgresConfig
 import psycopg2
 
 
 def get_db_connection():
     return psycopg2.connect(
-        dbname=config.PostgresConfig.dbname,
-        user=config.PostgresConfig.user,
-        password=config.PostgresConfig.password,
-        host=config.PostgresConfig.host,
-        port=config.PostgresConfig.port
+        dbname=PostgresConfig.dbname,
+        user=PostgresConfig.user,
+        password=PostgresConfig.password,
+        host=PostgresConfig.host,
+        port=PostgresConfig.port
     )
