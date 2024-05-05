ff218cc6a80247faf33a43599a859e95c2aaaf14
Youngha Park <youngha@Younghas-MacBook-Pro.local>
2024-05-05 15:53:54 +0900

    update main.py (add update_meta, update_media)

--- fast_api_server/main.py
+++ fast_api_server/main.py
@@ -15,9 +15,20 @@ def get_user_list():
     return user_list
 
 
+def update_meta(username):
+    pass
+
+
+def update_media(username):
+    pass
+
+
 def update():
     # 실행할 함수 내용을 여기에 작성합니다.
-    pass
+    user_list = get_user_list()
+    for username in user_list:
+        update_meta(username)
+        update_media(username)
 
 
 # 스케줄링할 작업을 추가합니다. 매일 새벽 1시에 job 함수를 실행하도록 예약합니다.
