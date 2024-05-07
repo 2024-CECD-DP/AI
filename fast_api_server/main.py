d343623bfc4558068fec5d5e647f601878e2c2c9
Youngha Park <youngha.uuhc@gmail.com>
2024-05-08 04:47:10 +0900

    update main.py

--- fast_api_server/main.py
+++ fast_api_server/main.py
@@ -1,5 +1,6 @@
 import requests
 import schedule
+import statistics
 
 from config import GraphAPIConfig
 from datetime import datetime
@@ -18,71 +19,107 @@ def get_user_list():
     return user_list
 
 
-def update_meta(username):
-    pass
-
-
-def update_media(insta_name):
-    conn, cur = get_db_connection()
-    cur.execute("SELECT * FROM influencer WHERE insta_name=%s", (insta_name,))
-    user_id = cur.fetchall()[0][0]
-
-    api_url = (
-        f"https://graph.facebook.com/v19.0/{GraphAPIConfig.user_id}"
-        f"?fields=business_discovery.username({insta_name})"
-        f"{{biography,name,username,follows_count,profile_picture_url,"
-        f"website,ig_id,followers_count,media_count,media.limit(40)"
-        f"{{caption,comments_count,id,children{{media_url,media_type}},"
-        f"like_count,media_product_type,media_type,media_url,owner,permalink,timestamp,username}}}}"
-        f"&access_token={GraphAPIConfig.access_token}"
-    )
-
-    response_json = requests.get(api_url).json()
-    media_list = response_json["business_discovery"]["media"]["data"]
-
-    for media in media_list:
-        try:
-            data = {
-                "collected_date": datetime.today().strftime("%Y-%m-%d"),
-                "comment_cnt": media["comments_count"],
-                "creation_date": media["timestamp"][:10],
-                "like_cnt": media["like_count"],
-                "user_id": user_id,
-                "caption": media["caption"].replace("\n", " "),
-                "insta_media_id": media["id"],
-            }
-
-            print(data)
-
-            insert_query = """
-                INSERT INTO media (collected_date, comment_cnt, creation_date, like_cnt, user_id, caption, insta_media_id)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-            """
-
-            cur.execute(insert_query, (
-                data["collected_date"],
-                data["comment_cnt"],
-                data["creation_date"],
-                data["like_cnt"],
-                data["user_id"],
-                data["caption"],
-                data["insta_media_id"]
-            ))
-
-            conn.commit()
-        except:
-            continue
-
-    cur.close()
-    conn.close()
-
-
 @app.get("/update")
-async def update():
+def update():
     user_list = get_user_list()
     for username in user_list:
-        update_meta(username)
-        update_media(username)
+        api_url = (
+            f"https://graph.facebook.com/v19.0/{GraphAPIConfig.user_id}"
+            f"?fields=business_discovery.username({username})"
+            f"{{biography,name,username,follows_count,profile_picture_url,"
+            f"website,ig_id,followers_count,media_count,media.limit(40)"
+            f"{{caption,comments_count,id,children{{media_url,media_type}},"
+            f"like_count,media_product_type,media_type,media_url,owner,permalink,timestamp,username}}}}"
+            f"&access_token={GraphAPIConfig.access_token}"
+        )
+
+        response_json = requests.get(api_url).json()
+
+        print(response_json)
+
+        # media 테이블 업데이트
+        conn, cur = get_db_connection()
+        cur.execute("SELECT * FROM influencer WHERE insta_name=%s", (username,))
+        user_id = cur.fetchall()[0][0]
+
+        media_list = response_json["business_discovery"]["media"]["data"]
+
+        for media in media_list:
+            try:
+                data = {
+                    "collected_date": datetime.today().strftime("%Y-%m-%d"),
+                    "comment_cnt": media["comments_count"],
+                    "creation_date": media["timestamp"][:10],
+                    "like_cnt": media["like_count"],
+                    "user_id": user_id,
+                    "caption": media["caption"].replace("\n", " "),
+                    "insta_media_id": media["id"],
+                }
+
+                print(data)
+
+                insert_query = """
+                    INSERT INTO media (collected_date, comment_cnt, creation_date, like_cnt, user_id, caption, insta_media_id)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """
+
+                cur.execute(insert_query, (
+                    data["collected_date"],
+                    data["comment_cnt"],
+                    data["creation_date"],
+                    data["like_cnt"],
+                    data["user_id"],
+                    data["caption"],
+                    data["insta_media_id"]
+                ))
+
+                conn.commit()
+            except:
+                continue
+
+        # meta 테이블 업데이트
+        collected_date = datetime.today().strftime("%Y-%m-%d")
+
+        cur.execute("SELECT * FROM media WHERE user_id=%s AND collected_date=%s", (user_id, collected_date,))
+
+        rows = cur.fetchall()
+
+        comment_cnt_list = []
+        like_cnt_list = []
+
+        for row in rows:
+            comment_cnt_list.append(row[1])
+            like_cnt_list.append(row[3])
+
+        # print(comment_cnt_list)
+        # print(like_cnt_list)
+
+        sql = """
+            INSERT INTO meta (collected_date, follower_cnt, comment_avg, like_avg, user_id)
+            VALUES (%s, %s, %s, %s, %s);
+        """
+
+        data = {
+            "collected_date": collected_date,
+            "follower_cnt": response_json["business_discovery"]["followers_count"],
+            "comment_avg": statistics.mean(comment_cnt_list),
+            "like_avg": statistics.mean(like_cnt_list),
+            "user_id": user_id
+        }
+
+        cur.execute(sql, (
+            data["collected_date"],
+            data["follower_cnt"],
+            data["comment_avg"],
+            data["like_avg"],
+            data["user_id"]
+        ))
+
+        # 변경사항 커밋
+        conn.commit()
+
+        cur.close()
+        conn.close()
 
 
 schedule.every().day.at("01:00").do(update)
