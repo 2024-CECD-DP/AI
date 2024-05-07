54b0d580f1def3d0f70b0ce4617a5b32a005d756
Youngha Park <youngha.uuhc@gmail.com>
2024-05-08 02:39:16 +0900

    update main.py (delete comments)

--- fast_api_server/main.py
+++ fast_api_server/main.py
@@ -24,14 +24,12 @@ def update_media(username):
 
 
 def update():
-    # 실행할 함수 내용을 여기에 작성합니다.
     user_list = get_user_list()
     for username in user_list:
         update_meta(username)
         update_media(username)
 
 
-# 스케줄링할 작업을 추가합니다. 매일 새벽 1시에 job 함수를 실행하도록 예약합니다.
 schedule.every().day.at("01:00").do(update)
 
 # @app.get("/")
